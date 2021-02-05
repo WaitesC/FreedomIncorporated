9b6879b920969a8938a3044683724141906c29d4
Chris <cwaites@hotmail.com>
2021-02-06 10:28:19 +1100

    Made a start on aiming/ shooting

---
+++ Freedom_Incorporated/Assets/Anama Scripts/Anama_Aim.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anama_Aim : MonoBehaviour
+{
+    Animator animator;
+
+    bool canAim;
+
+    float aimAngle;
+
+    public GameObject anamaArm;
+
+    public GameObject projectile;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        aimAngle = 0;
+
+        anamaArm.transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, 0f);
+
+    }
+
+    void Update()
+    {
+        ArmRotation();
+
+        //ShootProjectile();
+    }
+
+
+    void ArmRotation()
+    {
+        if (Input.GetAxis("Aim") > 0)
+        {
+            animator.SetBool("Aim", true);
+
+            anamaArm.transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, aimAngle);
+
+            ShootProjectile();
+        }
+
+        if (Input.GetAxis("Aim") <= 0)
+        {
+            animator.SetBool("Aim", false);
+
+            anamaArm.transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, 0f);
+        }
+
+        AimCalculator();
+    }
+
+    void AimCalculator()
+    {
+        float xAim = Input.GetAxis("HorizontalAim") * Time.deltaTime;
+        float yAim = Input.GetAxis("VerticalAim") * Time.deltaTime;
+
+        aimAngle = Mathf.Atan2(yAim, xAim) * Mathf.Rad2Deg +90;
+    }
+
+    void ShootProjectile()
+    {
+        if(Input.GetAxis("Shoot")>0)
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+
+            bullet.GetComponent<Rigidbody2D>().AddForce(transform.forward * 10);
+
+
+        }
+
+
+
+
+
+    }
+}
